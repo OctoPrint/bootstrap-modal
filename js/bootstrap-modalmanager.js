9bec78bf4835b4c35537342e352c74227538ba69
Gina Häußge <gina@octoprint.org>
2017-05-18 11:35:00 +0200

    Added patch notice to file headers

--- js/bootstrap-modalmanager.js
+++ js/bootstrap-modalmanager.js
@@ -14,6 +14,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * ==========================================================
+ * Patched version from https://github.com/OctoPrint/bootstrap-modal
+ *
+ *   - minHeight function added
  * ========================================================== */
 
 !function ($) {
